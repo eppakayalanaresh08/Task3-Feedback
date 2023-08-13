66ab7b73166c179a54af0be34b8977d597d5be8f
eppakayalanaresh <eppakayalanaresh08@gmail.com>
2023-08-13 15:47:44 +0530

    update

---
+++ src/app/feedback/feedback.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feedback',
+  templateUrl: './feedback.component.html',
+  styleUrls: ['./feedback.component.css']
+})
+export class FeedbackComponent {
+
+  isFeedbackSelected: boolean = false;
+
+  emojis = [
+    {
+      id: 0,
+      name: 'Sad',
+      imageUrl: 'https://assets.ccbp.in/frontend/react-js/sad-emoji-img.png'
+    },
+    {
+      id: 1,
+      name: 'None',
+      imageUrl: 'https://assets.ccbp.in/frontend/react-js/none-emoji-img.png'
+    },
+    {
+      id: 2,
+      name: 'Happy',
+      imageUrl: 'https://assets.ccbp.in/frontend/react-js/happy-emoji-img.png'
+    }
+    
+
+
+  ];
+  loveEmojiUrl: string = 'https://assets.ccbp.in/frontend/react-js/love-emoji-img.png';
+
+  onClickEmoji(): void {
+    this.isFeedbackSelected = true;
+  }
+
+}
